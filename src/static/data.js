0493599c2d39b4a29c2d7c5c8769ceccdffc12a2
Zoe Park <mary990t@gmail.com>
2022-12-16 00:49:05 +0900

    feat: dummy data added in static

---
+++ src/static/data.js
@@ -0,0 +1,26 @@
+export const dummyTodos = [
+    {
+        id: 1,
+        content:
+        'react 공부하기',
+        createdAt: '2022-02-24T16:17:47.000Z'
+    } ,
+    {
+        id: 2,
+        content:
+        '요가 가기',
+        createdAt: '2022-02-24T16:17:47.000Z'
+    } ,
+    {
+        id: 3,
+        content:
+        '빨래 하기',
+        createdAt: '2022-02-24T16:17:47.000Z'
+    } ,
+    {
+        id: 4,
+        content:
+        '과제 하기',
+        createdAt: '2022-02-24T16:17:47.000Z'
+    } ,
+]
